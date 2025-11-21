3259e9684a5305552923e41b665d689dfb898c04
Riccardo <richi-97@live.it>
2025-11-21 11:06:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "./App.css";
+import MyNav from "./components/MyNav";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <MyNav />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
