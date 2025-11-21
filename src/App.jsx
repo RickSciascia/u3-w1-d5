9d300f565a8cb2eb6b740ce0544cf7d706d63cc7
Riccardo <richi-97@live.it>
2025-11-21 15:53:38 +0100

    removed StaticHead component from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "./App.css";
 import MyNav from "./components/MyNav";
 import Footer from "./components/Footer";
-import StaticHead from "./components/StaticHead";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 
